9232c329b9f07d79e7512c8d6424d1b862c1feed
Habeeb <habeebahmadu1@gmail.com>
2023-11-16 13:00:34 +0100

    feat: setup tailwind css and shadcn/ui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,5 @@
 const MainPage = () => {
-  return <div className="bg-red-600 text-red-400">MainPageerrerrrer</div>;
+  return <div className="bg-red-600 text-red-400">MainPagefdfdfd</div>;
 };
 
 export default MainPage;
